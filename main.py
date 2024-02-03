503ee5ceebcf7593578fea42b5f34badbece5862
AamirAbatiyow <150764416+AamirAbatiyow@users.noreply.github.com>
2024-02-03 13:17:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 1000
 screen = pygame.display.setmode((SCREEN_WIDTH,SCREEN_HEIGHT))
+<<<<<<< HEAD
 player = pygame.image.load(os.path.join("Assets", "Player.png"))
 player = pygame.transform.scale(player, (600,600))
 
 def draw_player(x, y):
     screen.blit(player, (x,y))
+=======
+player_sprite = pygame.image.load(os.path.join("Assets", "Ball.jpg"))
+player_sprite = pygame.transform.scale(player_sprite, (600,600))
+
+def draw_ball(x, y):
+    screen.blit(player_sprite, (x,y))
+>>>>>>> b7266c8090422ea2cee31905d64916ba45609a3f
 run = True
 while run:
     for event in pygame.event.get():
