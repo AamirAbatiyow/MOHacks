243698f2f4f5ba0deb4f6c91255ccd3a103933e8
tclothier373 <143123140+tclothier373@users.noreply.github.com>
2024-02-03 13:40:19 -0600

    attempted to draw player

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 import pygame
 import os
 pygame.init()
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 1000
-screen = pygame.display.setmode((SCREEN_WIDTH,SCREEN_HEIGHT))
-player_sprite = pygame.image.load(os.path.join("Assets", "Ball.jpg"))
-player_sprite = pygame.transform.scale(player_sprite, (600,600))
-
-def draw_ball(x, y):
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 700
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+player_sprite = pygame.image.load(os.path.join("Assets", "player.png"))
+player_sprite = pygame.transform.scale(player_sprite, (100,100))
+player_x =400
+player_y =500 
+class Background(pygame.sprite.Sprite):
+    def __init__(self, image_file, location):
+        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+def draw_player(x, y):
     screen.blit(player_sprite, (x,y))
+BackGround = Background('bg.png',[0,0])
 run = True
 while run:
+    screen.fill([255, 255, 255])
+    screen.blit(BackGround.image, BackGround.rect)
+    draw_player(player_x,player_y)
+
+
+
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 pygame.quit()
+
