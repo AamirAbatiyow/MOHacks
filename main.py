269afa4423f0ab9310b6349eb18ed721bf5457cb
AamirAbatiyow <150764416+AamirAbatiyow@users.noreply.github.com>
2024-02-09 12:50:52 -0600

    aliens spawn from both sides, aliens get faster as game progresses, score, life, game end, and calculated score

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 #imports libraries
 import pygame
 import os
+import random
 #initializes the pygame thing
 pygame.init()
 #declaring all our classes
@@ -124,6 +125,7 @@ clock = pygame.time.Clock()
 gravity = 1
 jump_velocity = -22
 run = True
+alien_speed = 1
 #creates instances of all our classes
 charge1 = Charge(250, 400, "charge.png")
 charge2 = Charge(200, 400, "charge.png")
@@ -140,7 +142,8 @@ tile6 = Tile(500, 600, "tile.png")
 tile7 = Tile(600, 600, "tile.png")
 tile8 = Tile(700, 600, "tile.png")
 tile9 = Tile(800, 600, "tile.png")
-
+aliens_defeated = 0
+life = 3
 while run:
     clock.tick(60)
     screen.blit(bg, (0, 0))
@@ -149,9 +152,18 @@ while run:
     draw_charges_and_tiles()
     draw_energy(energy_x, energy_y)
     draw_alien(alien_x, alien_y)
+    # Display clock at the top
+    time_passed = pygame.time.get_ticks() // 1000
+    time_text = font.render(f"Time: {time_passed}", True, (255, 255, 255))
+    aliens_defeated_text = font.render(f"Aliens: {aliens_defeated}", True, (255, 255, 255))
+    life_text = font.render(f"Life: {life}", True, (255, 255, 255))
+    screen.blit(time_text, (425, 15))
+    screen.blit(aliens_defeated_text, (15, 15))
+    screen.blit(life_text, (800, 15))
     # Update and draw beams
     updated_beams = []
     beam_rects = []
+    alien_side = random.choice(['left', 'right'])
     for beam in beams:
         beam.update()
         screen.blit(beam.image, beam.rect.topleft)
@@ -159,13 +171,29 @@ while run:
     alien_rect = alien.get_rect(topleft=(alien_x, alien_y))
     for beam in beams:
         if alien_rect.colliderect(beam.rect):
-            alien_x = -100
+            if alien_side == 'left':
+                alien_x = -100
+            if alien_side == 'right':
+                alien_x = 1000
             alien_y = 500
+            aliens_defeated += 1
             print("Alien hit!")
             HIT_SOUND.play()
             updated_beams.remove(beam)
     beams = updated_beams
     player_rect = player.get_rect(topleft=(player_x, player_y))
+    if player_rect.colliderect(alien_rect):
+        life -= 1
+        print("Player hit!")
+        HIT_SOUND.play()
+        if alien_side == 'left':
+            alien_x = -100
+        if alien_side == 'right':
+           alien_x = 1000
+        alien_y = 500
+        player_x = 400
+        player_y = 500
+        energy_level = 119
     # Check collisions with charge blocks
     for charge in [charge1, charge2, charge3, charge4, charge5, charge6]:
         if player_rect.colliderect(charge.rect) and player_rect.bottom <= charge.rect.top + 5:
@@ -194,12 +222,23 @@ while run:
         energy_sprite = pygame.transform.scale(energy2, (125, 50))
     elif energy_level < 20:
         energy_sprite = pygame.transform.scale(energy1, (125, 50))
+    
+    
+    if time_passed >= 60:
+        alien_speed = 5
+    elif time_passed >= 45:
+        alien_speed = 4
+    elif time_passed >= 30:
+        alien_speed = 3
+    elif time_passed >= 15:
+        alien_speed = 2
+
 
     #alien follows the player's x position
     if alien_x > player_x:
-        alien_x -= 2
+        alien_x -= alien_speed
     else:
-        alien_x += 2
+        alien_x += alien_speed
     #button presses(you have to hit them once and you cant press down)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -217,7 +256,7 @@ while run:
                 LASER_SOUND.play()
     #button presses(you must hold them down)
     pressed_keys = pygame.key.get_pressed()
-    if pressed_keys[pygame.K_a]:
+    if pressed_keys[pygame.K_a] and player_x > 0:
         if (WALK_SOUND.get_num_channels() == 0):
             WALK_SOUND.play()
         player_x -= speed
@@ -225,18 +264,16 @@ while run:
     
         playerFacesRight = False
 
-    if pressed_keys[pygame.K_d]:
+    if pressed_keys[pygame.K_d] and player_x < SCREEN_WIDTH - 100:
         player_x += speed
         if (WALK_SOUND.get_num_channels() == 0):
             WALK_SOUND.play()
         player = pygame.transform.scale(player_right, (100, 100))
         playerFacesRight = True
 
-    # Display clock at the top
-    time_passed = pygame.time.get_ticks() // 1000
-    time_text = font.render(f"Time: {time_passed}", True, (255, 255, 255))
-    screen.blit(time_text, (425, 15))
-
+    if (life == 0):
+        score = (time_passed*100) + (aliens_defeated*500)
+        print("Game Over. You survived for " + str(time_passed)+" seconds. You defeated " + str(aliens_defeated)+ " aliens. Your score was: " + str(score))
+        run = False
     pygame.display.update()
-
 pygame.quit()
